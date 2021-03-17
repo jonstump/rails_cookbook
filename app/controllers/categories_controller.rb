f0806fedc46c3ed759c97e67512acbd47c8f908a
Jon Stump <jmstump@gmail.com>
2021-03-17 15:53:38 -0700

    add CRUD for categories
    Co-authored-by: Kerry Lang <klang812@gmail.com>

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,50 @@
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+    render :index
+  end
+
+  def new
+    @category = Category.new
+    render :new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to categories_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+    render :edit
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    render :show
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to categories_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id]).destroy
+    redirect_to categories_path
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:category_name)
+    end
+
+end
