65682ef82dbf5442a6b594c55dc317bccda7c29e
Kerry Lang <klang812@gmail.com>
2021-03-17 11:01:44 -0700

    add validation for recipe name test to pass
    Co-authored-by: Jon Stump <jmstump@gmail.com>

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,20 @@
+class RecipeController < ApplicationController
+  def index
+    @recipes = Recipe.all
+    render :index
+  end
+
+  def new
+    @recipe = Recipe.new
+    render :new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to recipe_path
+    else
+      render :new
+    end
+  end
+end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 describe Recipe do
   it {should have_and_belong_to_many(:ingredients)}
   it {should have_and_belong_to_many(:categories)}
+  it {should validate_presence_of :name}
 end
