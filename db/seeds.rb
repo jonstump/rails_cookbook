c8aa6fc5b4120c54b2423d267f9f812ca4550e32
Jon Stump <jmstump@gmail.com>
2021-03-18 13:51:23 -0700

    add seed with faker to DB
    Co-authored-by: Kerry Lang <klang812@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Recipe.destroy_all
+Category.destroy_all
+Ingredient.destroy_all
+
+10.times do
+  @recipe = Recipe.create!(name: Faker::Food.unique.dish,    
+    instructions: Faker::Lorem.sentence(word_count: 20).chop)
+  1.times do
+    @ingredient1 = Ingredient.create!(ingredient_name: Faker::Food.unique.ingredient)
+    @category1 = Category.create!(category_name: Faker::Food.fruits)
+    @recipe.ingredients << @ingredient1
+    @recipe.categories << @category1
+  end
+end
+
+
+p "Created #{Recipe.count} recipes, #{Ingredient.count} ingredients, and #{Category.count} categories."
