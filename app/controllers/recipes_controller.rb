d6dc12691bac1ec4b5e654809ad98f81ad76fcd4
Jon Stump <jmstump@gmail.com>
2021-03-18 11:34:46 -0700

    add connections between categories, ingredients, recipies
    Co-authored-by: Kerry Lang <klang812@gmail.com>

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,17 +1,30 @@
 class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
+    @categories = Category.all
+    @ingredients = Ingredient.all
     render :index
   end
 
   def new
     @recipe = Recipe.new
+    @categories = Category.all
+    @ingredients = Ingredient.all
     render :new
   end
 
   def create
     @recipe = Recipe.new(recipe_params)
     if @recipe.save
+      if params[:category_selection].to_i != 0
+        category = Category.find(params[:category_selection].to_i)
+        binding.pry
+        category.recipes << @recipe
+      end
+      if params[:ingredient_selection].to_i != 0
+        ingredient = Ingredient.find(params[:ingredient_selection].to_i)
+        ingredient.recipes << @recipe
+      end
       redirect_to recipes_path
     else
       render :new
@@ -20,17 +33,29 @@ class RecipesController < ApplicationController
 
   def edit
     @recipe = Recipe.find(params[:id])
+    @categories = Category.all
+    @ingredients = Ingredient.all
     render :edit
   end
 
   def show
     @recipe = Recipe.find(params[:id])
+    @ingredients = Ingredient.all
+    @categories = Category.all
     render :show
   end
 
   def update
     @recipe = Recipe.find(params[:id])
     if @recipe.update(recipe_params)
+      if params[:category_selection].to_i != 0
+        category = Category.find(params[:category_selection].to_i)
+        category.recipes << @recipe
+      end
+      if params[:ingredient_selection].to_i != 0
+        ingredient = Ingredient.find(params[:ingredient_selection].to_i)
+        ingredient.recipes << @recipe
+      end
       redirect_to recipes_path
     else
       render :edit
--- app/models/category_recipe.rb
+++
@@ -1,4 +0,0 @@
-class CategoryRecipe < ApplicationRecord
-  belongs_to :recipe
-  belongs_to :category
-end
