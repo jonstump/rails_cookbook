85a66f62c1635df3d403fd433a44231e13b6d375
Jon Stump <jmstump@gmail.com>
2021-03-18 16:14:40 -0700

    passing integration test for category edit error message
    Co-authored-by: Kerry Lang <klang812@gmail.com>

--- spec/features/edit_category_pages_spec.rb
+++ spec/features/edit_category_pages_spec.rb
@@ -13,9 +13,14 @@ describe "the edit category process" do
     expect(page).to have_content 'pasta'
   end
 
-  # it "gives an error when no category name is entered" do
-  #   visit new_category_path
-  #   click_on 'Create Category'
-  #   expect(page).to have_content "Category name can't be blank"
-  # end
+  it "gives an error when no category name is entered" do
+    category = Category.new({:category_name => 'bread'})
+    category.save()
+    visit categories_path
+    click_link 'bread'
+    click_link 'Edit'
+    fill_in 'Category name', :with => ' '
+    click_on 'Update Category'
+    expect(page).to have_content "Category name can't be blank"
+  end
 end
